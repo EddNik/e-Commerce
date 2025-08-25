e458f3288f720b2ea68e689aaf32a9ae932d1250
Eduard Vyskrebtsov <33968701+EddNik@users.noreply.github.com>
2025-08-25 20:58:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-import{u as t,a as r,r as a}from"./assets/modal-BTTa0Kjh.js";import{g as e,a as o,b as s,o as u,c as g}from"./assets/handlers-CuxqnO0I.js";import"./assets/vendor-4yCzdkXl.js";e();o();s();u();t();r();g();a.formSearch.reset();
+// import{u as t,a as r,r as a}from"./assets/modal-BTTa0Kjh.js";import{g as e,a as o,b as s,o as u,c as g}from"./assets/handlers-CuxqnO0I.js";import"./assets/vendor-4yCzdkXl.js";e();o();s();u();t();r();g();a.formSearch.reset();
 //# sourceMappingURL=index.js.map
